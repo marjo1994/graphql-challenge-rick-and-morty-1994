803b9c4955ab0bc9165d861881c6bef792e141e6
marjo1994 <m_rhl@live.com>
2025-09-05 17:28:24 -0500

    feat(CharacterDetails): Improving Character Details, adding episodes and overflow vertically

--- src/components/CharacterDetails/CharacterDetail.tsx
+++ src/components/CharacterDetails/CharacterDetail.tsx
@@ -24,35 +24,54 @@ export const CharacterDetails = ({ characterId }: Props) => {
 
   const char = data?.character;
   const episodes = char.episode?.slice(0, 5) || [];
-  console.log(episodes);
 
   return (
     <div className={styles.characterDetails}>
       <h2 className={styles.subtitle}>General Information</h2>
-      <div className={styles.detail}>
-        <p className={styles.label}>Name</p>
-        <p className={styles.info}>{char.name}</p>
-      </div>
-      <div className={styles.detail}>
-        <p className={styles.label}>Species</p>
-        <p className={styles.info}>{char.species}</p>
-      </div>
-      <div className={styles.detail}>
-        <p className={styles.label}>Status</p>
-        <p className={styles.info}>{char.status}</p>
-      </div>
-      <div className={styles.detail}>
-        <p className={styles.label}>Gender</p>
-        <p className={styles.info}>{char.gender}</p>
-      </div>
-      <div className={styles.detail}>
-        <p className={styles.label}>Origin</p>
-        <p className={styles.info}>{char.origin?.name}</p>
-      </div>
-      <div className={styles.detail}>
-        <p className={styles.label}>Location</p>
-        <p className={styles.info}>{char.location?.name}</p>
-      </div>
+      {char.image && (
+        <img
+          src={char.image}
+          alt={char.name ?? `Rick and Morty Character`}
+          loading="lazy"
+        />
+      )}
+      {char.name && (
+        <div className={styles.detail}>
+          <p className={styles.label}>Name</p>
+          <p className={styles.info}>{char.name}</p>
+        </div>
+      )}
+      {char.species && (
+        <div className={styles.detail}>
+          <p className={styles.label}>Species</p>
+          <p className={styles.info}>{char.species}</p>
+        </div>
+      )}
+      {char.status && (
+        <div className={styles.detail}>
+          <p className={styles.label}>Status</p>
+          <p className={styles.info}>{char.status}</p>
+        </div>
+      )}
+      {char.gender && (
+        <div className={styles.detail}>
+          <p className={styles.label}>Gender</p>
+          <p className={styles.info}>{char.gender}</p>
+        </div>
+      )}
+      {char.origin && (
+        <div className={styles.detail}>
+          <p className={styles.label}>Origin</p>
+          <p className={styles.info}>{char.origin?.name}</p>
+        </div>
+      )}
+      {char.location && (
+        <div className={styles.detail}>
+          <p className={styles.label}>Location</p>
+          <p className={styles.info}>{char.location?.name}</p>
+        </div>
+      )}
+
       <h2 className={styles.secondSubtitle}>Episodies</h2>
       {episodes.length > 0 ? (
         episodes.map((episode) => (
